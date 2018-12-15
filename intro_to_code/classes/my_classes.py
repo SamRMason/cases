ea84f44f846234135944a71519dd499bd3d1a4fe
Sam Mason <sam.r.mason@gmail.com>
2018-12-14 19:14:00 -0700

    initial commi

---
+++ intro_to_code/classes/my_classes.py
@@ -0,0 +1,17 @@
+class Person:
+    species = "homosapien"
+
+    def __init__(self, species = "homosapien", age=21, name_first="McLovin", name_last="Jones"):
+        self.species = species
+        self.age = age
+        self.name_first = name_first
+        self.name_last = name_last
+
+class Student(Person):
+
+    def __init__(self, species = "homosapien", age=21, name_first="McLovin", name_last="Jones", student_id):
+        Person.__init__(species, age, name_first, name_last)
+        self.student_id = student_id
+
+class Teacher(Person)
+    
